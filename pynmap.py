eea9b788eae90aacb0f6a3242bedf011fde9af6f
Lucas Zanella <me@lucaszanella.com>
2018-01-08 15:58:47 -0200

    small updates

--- pynmap.py
+++ pynmap.py
@@ -5,7 +5,10 @@ import ipaddress
 from threading import Thread
 import time
 
-delay = 0.01
+#Maximum threads concurrently running is default_delay*default_timeout
+default_delay = 0.01 #Delay between thread launches
+default_timeout = 3 #Timeout for each thread
+
 
 def return_range_of_ips(ips):
   return [str(x) for x in list(ipaddress.ip_network(ips))]
@@ -13,10 +16,9 @@ def return_range_of_ips(ips):
 def scan_(socket, address, port, result):
 	c = socket.connect_ex((address, port))
 	result[address + ":" + str(port)] = c
-	#print(result)
 
 class Nmap(object):
-	def __init__(self, socket=None, timeout=3):
+	def __init__(self, socket=None, default_timeout=3):
 		if socket is None:
 		    self.socket = socket_.socket(socket_.AF_INET, socket_.SOCK_STREAM)
 		else:
@@ -36,8 +38,8 @@ class Nmap(object):
 				t = Thread(target=scan_, args=(self.socket, address, port, result,))
 				t.start()
 				threads.append(t)
-				time.sleep(delay)
+				time.sleep(default_delay)
 		for thread in threads:
-			thread.join()
+			thread.join()#Waits for all threads to stop so we return all results
 		self.socket.close()
 		return result
--- test.py
+++ test.py
@@ -4,5 +4,5 @@
 from pynmap import *
 
 nmap = Nmap()
-r = nmap.scan(addresses="192.168.1.2", ports=80)
+r = nmap.scan(addresses="192.168.1.2/24", ports=80)
 print(r)

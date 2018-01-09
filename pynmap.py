d36ba7510c51601fef32d7b826f5d8b499320f62
Lucas Zanella <me@lucaszanella.com>
2018-01-09 12:23:47 -0200

    fixed proxy problems

--- pynmap.py
+++ pynmap.py
@@ -1,29 +1,42 @@
 #Lucas Zanella
 
-import socket as socket_
+import socket as socket
 import ipaddress
 from threading import Thread
 import time
+import copy
+import socks
 
 #Maximum threads concurrently running is default_delay*default_timeout
 default_delay = 0.01 #Delay between thread launches
 default_timeout = 3 #Timeout for each thread
 
-
 def return_range_of_ips(ips):
   return [str(x) for x in list(ipaddress.ip_network(ips))]
 
 def scan_(socket, address, port, result):
+	#print(socket)
+	#print(socket.connect((address, port)))
 	c = socket.connect_ex((address, port))
+	#print(c)
+	#print(address + ":" + str(port) + " - " + str(c))
 	result[address + ":" + str(port)] = c
+	
+
 
 class Nmap(object):
-	def __init__(self, socket=None, default_timeout=3):
-		if socket is None:
-		    self.socket = socket_.socket(socket_.AF_INET, socket_.SOCK_STREAM)
-		else:
-		    self.socket = socket
-		self.socket.settimeout(default_timeout)
+	def __init__(self, proxy=None, default_timeout=3):
+		self.proxy = proxy
+		self.default_timeout = default_timeout
+		
+	def new_socket(self):
+	        if not self.proxy:
+	                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	        else:
+                        s = socks.socksocket()
+                        s.set_proxy(socks.SOCKS5, self.proxy['socks_host'], self.proxy['socks_port'])
+	        s.settimeout(self.default_timeout) 
+	        return s
 
 	def scan(self, addresses=None, ports=None):
 		result = {}
@@ -34,8 +47,8 @@ class Nmap(object):
 			ports = [ports]
 		for address in return_range_of_ips(addresses):
 			for port in ports:
-				print("trying " + address + ":" + str(port))
-				t = Thread(target=scan_, args=(self.socket, address, port, result,))
+				#print("trying " + address + ":" + str(port))
+				t = Thread(target=scan_, args=(self.new_socket(), address, port, result,))
 				t.start()
 				threads.append(t)
 				time.sleep(default_delay)
